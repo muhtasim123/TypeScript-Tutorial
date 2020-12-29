49526c1675f06ae71fff92fc5fb7ce18aa9cef01
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2020-12-29 09:56:16 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+let x:number = 5
+
+let y = 5
+
+let s:string
+
+let b:boolean
+
+let arr:number[] = [0, 10, 20]
+
+let keyValue: [string, object] = ["name", {}]
+
+let xypos: [number, number]
+
+let n:null
+
+let un:undefined
+
+let func: (f)=>void
+
